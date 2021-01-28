459a2b6fbb95fdbce2ffef3c13bddc68e89ee0b7
gracielo <alex-c19@hotmail.com>
2021-01-28 16:20:30 -0600

    Initial version of project

---
+++ ApplicantTrackerServicesExcel/src/main/java/com/tcs/trade/controller/CandidateExcelController.java
@@ -0,0 +1,207 @@
+package com.tcs.trade.controller;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tcs.trade.model.CandidateExcel;
+
+@RestController
+public class CandidateExcelController {
+
+	Workbook candidatesWorkBook;
+	FileInputStream excelFile;
+	private static final String path = "C:\\Users\\ALEJANDROBARRETOJIME\\Documents\\workspace-spring-tool-suite-4-4.9.0.RELEASE\\ApplicantTrackerServicesExcel\\src\\main\\resources\\CandidatesTracker.xlsx";
+	private static final String sheetName = "Candidates";
+
+	@PostMapping("/registerCandidate")
+	public ResponseEntity<CandidateExcel> registerCandidate(@RequestBody CandidateExcel candidate) {
+		try {
+			if (candidate.getStatus() == null) {
+				candidate.setStatus("IE Pending");
+			}
+			excelFile = new FileInputStream(new File(path));
+			candidatesWorkBook = new XSSFWorkbook(excelFile);
+			Sheet worksheet = candidatesWorkBook.getSheet(sheetName);
+			int rowIndex = worksheet.getLastRowNum() + 1;
+			Row row = worksheet.createRow(rowIndex);
+			Cell cell = row.createCell(0);
+			cell.setCellValue(candidate.getName());
+			cell = row.createCell(1);
+			cell.setCellValue(candidate.getEmail());
+			cell = row.createCell(2);
+			cell.setCellValue(candidate.getPhone());
+			cell = row.createCell(3);
+			cell.setCellValue(candidate.getProfile());
+			cell = row.createCell(4);
+			cell.setCellValue(candidate.getYearsOfExperience());
+			cell = row.createCell(5);
+			cell.setCellValue(candidate.getEnglishLevel());
+			cell = row.createCell(6);
+			cell.setCellValue(candidate.getStatus());
+			cell = row.createCell(7);
+			cell.setCellValue(this.getSystemDate());
+			cell = row.createCell(8);
+			cell.setCellValue(candidate.getGrade());
+			cell = row.createCell(9);
+			cell.setCellValue(candidate.getEvaluator());
+			cell = row.createCell(10);
+			cell.setCellValue(candidate.getFeedback());
+
+			FileOutputStream output = new FileOutputStream(path);
+			candidatesWorkBook.write(output);
+			candidatesWorkBook.close();
+			return new ResponseEntity<>(candidate, HttpStatus.CREATED);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(null, HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	private String getSystemDate() {
+		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+		LocalDateTime now = LocalDateTime.now();
+		return dtf.format(now);
+	}
+
+	@GetMapping("/getCandidateInfoById/{email}")
+	public ResponseEntity<Object> getCandidateInfoById(@PathVariable("email") String email) {
+		try {
+			CandidateExcel can = new CandidateExcel();
+			excelFile = new FileInputStream(new File(path));
+			candidatesWorkBook = new XSSFWorkbook(excelFile);
+			Sheet worksheet = candidatesWorkBook.getSheet(sheetName);
+			Iterator<Row> data = worksheet.iterator();
+			while (data.hasNext()) {
+				Row currentRow = data.next();
+				String aux = currentRow.getCell(1).getStringCellValue();
+				if (aux.compareToIgnoreCase(email) == 0) {
+					can.setName(currentRow.getCell(0).getStringCellValue());
+					can.setEmail(currentRow.getCell(1).getStringCellValue());
+					can.setPhone((int) currentRow.getCell(2).getNumericCellValue());
+					can.setProfile(currentRow.getCell(3).getStringCellValue());
+					can.setYearsOfExperience((int) currentRow.getCell(4).getNumericCellValue());
+					can.setEnglishLevel(currentRow.getCell(5).getStringCellValue());
+					can.setStatus(currentRow.getCell(6).getStringCellValue());
+					can.setCreationDate(new Date(currentRow.getCell(7).getStringCellValue()));
+					can.setGrade((int) currentRow.getCell(8).getNumericCellValue());
+					can.setEvaluator(currentRow.getCell(9).getStringCellValue());
+					can.setFeedback(currentRow.getCell(10).getStringCellValue());
+				}
+			}
+			candidatesWorkBook.close();
+			return new ResponseEntity<>(can, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+	@PutMapping("/updateCandidate/{email}")
+	public ResponseEntity<Object> updateCandidate(@RequestBody CandidateExcel candidate,
+			@PathVariable("email") String email) {
+		try {
+			excelFile = new FileInputStream(new File(path));
+			candidatesWorkBook = new XSSFWorkbook(excelFile);
+			Sheet worksheet = candidatesWorkBook.getSheet(sheetName);
+			Iterator<Row> data = worksheet.iterator();
+			while (data.hasNext()) {
+				Row row = data.next();
+
+				String aux = row.getCell(1).getStringCellValue();
+				if (aux.compareToIgnoreCase(email) == 0) {
+					Cell cell = row.getCell(0);
+					cell.setCellValue(candidate.getName() != null ? candidate.getName() : cell.getStringCellValue());
+					cell = row.getCell(1);
+					cell.setCellValue(candidate.getEmail() != null ? candidate.getEmail() : cell.getStringCellValue());
+					cell = row.getCell(2);
+					cell.setCellValue(candidate.getPhone() != 0 ? candidate.getPhone() : cell.getNumericCellValue());
+					cell = row.getCell(3);
+					cell.setCellValue(
+							candidate.getProfile() != null ? candidate.getProfile() : cell.getStringCellValue());
+					cell = row.getCell(4);
+					cell.setCellValue(candidate.getYearsOfExperience() != 0 ? candidate.getYearsOfExperience()
+							: cell.getNumericCellValue());
+					cell = row.getCell(5);
+					cell.setCellValue(candidate.getEnglishLevel() != null ? candidate.getEnglishLevel()
+							: cell.getStringCellValue());
+					cell = row.getCell(6);
+					cell.setCellValue(
+							candidate.getStatus() != null ? candidate.getStatus() : cell.getStringCellValue());
+					cell = row.getCell(8);
+					cell.setCellValue(candidate.getGrade() != 0 ? candidate.getGrade() : cell.getNumericCellValue());
+					cell = row.getCell(9);
+					cell.setCellValue(
+							candidate.getEvaluator() != null ? candidate.getEvaluator() : cell.getStringCellValue());
+					cell = row.getCell(10);
+					cell.setCellValue(candidate.getFeedback());
+				}
+
+			}
+			FileOutputStream output = new FileOutputStream(path);
+			candidatesWorkBook.write(output);
+			candidatesWorkBook.close();
+			return new ResponseEntity<>(candidate, HttpStatus.OK);
+		} catch (Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+	}
+
+	@GetMapping("/getCandidates")
+	public ResponseEntity<Object> getCandidates() {
+		try {
+			CandidateExcel can;
+			List<CandidateExcel> candidates = new ArrayList<>();
+			excelFile = new FileInputStream(new File(path));
+			candidatesWorkBook = new XSSFWorkbook(excelFile);
+			Sheet worksheet = candidatesWorkBook.getSheet(sheetName);
+			Iterator<Row> data = worksheet.iterator();
+			data.next();
+			while (data.hasNext()) {
+				Row currentRow = data.next();
+				can = new CandidateExcel();
+				can.setName(currentRow.getCell(0).getStringCellValue());
+				can.setEmail(currentRow.getCell(1).getStringCellValue());
+				can.setPhone((int)currentRow.getCell(2).getNumericCellValue());
+				can.setProfile(currentRow.getCell(3).getStringCellValue());
+				can.setYearsOfExperience((int) currentRow.getCell(4).getNumericCellValue());
+				can.setEnglishLevel(currentRow.getCell(5).getStringCellValue());
+				can.setStatus(currentRow.getCell(6).getStringCellValue());
+				can.setCreationDate(new Date(currentRow.getCell(7).getStringCellValue()));
+				can.setGrade((int) currentRow.getCell(8).getNumericCellValue());
+				can.setEvaluator(currentRow.getCell(9).getStringCellValue());
+				can.setFeedback(currentRow.getCell(10).getStringCellValue());
+				candidates.add(can);
+			}
+			candidatesWorkBook.close();
+			return new ResponseEntity<>(candidates.stream().filter(cand->!"inactive".equals(cand.getStatus())).collect(Collectors.toList()), HttpStatus.OK);
+		} catch (
+
+		Exception e) {
+			e.printStackTrace();
+			return new ResponseEntity<>(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+	}
+
+}
---
+++ ApplicantTrackerServicesExcel/src/main/java/com/tcs/trade/model/CandidateExcel.java
@@ -0,0 +1,88 @@
+package com.tcs.trade.model;
+
+import java.util.Date;
+
+public class CandidateExcel {
+		
+	private String name;
+	private String email;	
+	private int phone;	
+	private String profile;		
+	private int yearsOfExperience;	
+	private String englishLevel;	
+	private String status;	
+	private Date creationDate;
+	private int grade;
+	private String evaluator;
+	private String feedback;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public String getProfile() {
+		return profile;
+	}
+	public void setProfile(String profile) {
+		this.profile = profile;
+	}
+	public int getYearsOfExperience() {
+		return yearsOfExperience;
+	}
+	public void setYearsOfExperience(int yearsOfExperience) {
+		this.yearsOfExperience = yearsOfExperience;
+	}
+	public String getEnglishLevel() {
+		return englishLevel;
+	}
+	public void setEnglishLevel(String englishLevel) {
+		this.englishLevel = englishLevel;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
+	}
+	public Date getCreationDate() {
+		return creationDate;
+	}
+	public void setCreationDate(Date creationDate) {
+		this.creationDate = creationDate;
+	}
+	public int getGrade() {
+		return grade;
+	}
+	public void setGrade(int grade) {
+		this.grade = grade;
+	}
+	public String getEvaluator() {
+		return evaluator;
+	}
+	public void setEvaluator(String evaluator) {
+		this.evaluator = evaluator;
+	}
+	public String getFeedback() {
+		return feedback;
+	}
+	public void setFeedback(String feedback) {
+		this.feedback = feedback;
+	}
+	
+	
+	
+}
