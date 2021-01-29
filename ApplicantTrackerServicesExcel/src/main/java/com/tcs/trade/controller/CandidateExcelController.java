a67f38394ab385bfc80a8f70da7f5e359c141122
gracielo <alex-c19@hotmail.com>
2021-01-28 22:52:32 -0600

    1111

--- ApplicantTrackerServicesExcel/src/main/java/com/tcs/trade/controller/CandidateExcelController.java
+++ ApplicantTrackerServicesExcel/src/main/java/com/tcs/trade/controller/CandidateExcelController.java
@@ -224,7 +224,7 @@ public class CandidateExcelController {
 
 	}
 	
-	@GetMapping("createFile")
+	@GetMapping("/createFile")
 	private void createFile() {
 		try {
 		classLoader= this.getClass().getClassLoader();
